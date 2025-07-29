f8e89f0c1be2d68a958d20e26caa5b672dc288fe
Devaansh Sai <2420030787@klh.edu.in>
2025-07-29 14:09:48 +0530

    Add files via upload

---
+++ skill/WrapperClass.java
@@ -0,0 +1,13 @@
+package skill;
+
+public class WrapperClass {
+	 public static void main(String[] args) {
+	        String str = "123";
+	        int num = Integer.parseInt(str); 
+
+	        System.out.printf("Original string: %s\n", str);
+	        System.out.printf("Converted int: %d\n", num);
+	    }
+	}
+
+
---
+++ skill/typeconversion.java
@@ -0,0 +1,12 @@
+package skill;
+
+public class typeconversion {
+	 public static void main(String[] args) {
+	        int num = 100;
+	        float f = num; 
+
+	        System.out.printf("Original int value: %d\n", num);
+	        System.out.printf("After widening to float: %.2f\n", f);
+	    }
+
+}
