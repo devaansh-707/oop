507a261162a43602c2ac9f485ccc71b87fa5684f
Devaansh Sai <2420030787@klh.edu.in>
2025-07-29 13:52:01 +0530

    All text file programs

---
+++ widening.java
@@ -0,0 +1,16 @@
+package Project1st;
+
+public class widening {
+
+	public static void main(String[] args) {
+		  int a = 100;
+	        long b = a;
+	        float c = b;
+
+	        System.out.println("int: " + a);
+	        System.out.println("long: " + b);
+	        System.out.println("float: " + c); 
+	        
+	}
+
+}
